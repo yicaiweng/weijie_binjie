0df76316837ebaa846584ec7b8e3ab6845a6b004
Yicai Weng <yicaiweng@Yicais-iMac.local>
2017-03-29 11:10:37 -0500

    added slidshow

---
+++ js/slideshow.js
@@ -0,0 +1,17 @@
+var index = 0;
+carousel();
+
+function carousel(){
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  for(i = 0; i <x.length; i++){
+    x[i].style.display="none";
+  }
+
+  index++;
+  if(index > x.length) {
+    index = 1;
+  }
+  x[index - 1].style.display="block";
+  setTimeout(carousel, 10000);
+}
